702cd4d15551640fa4ced20bbda8f2403e2e4260
Yunseok <watemark1017@gmail.com>
2025-07-17 19:57:05 +0900

    마크다운 추가 + 검색 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from ..models import Question
 
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
     # context = {'question_list': question_list}
+    
+    if kw:
+        # subject__contains=kw 대신 subject__icontains=kw을 사용하면 대소문자를 가리지 않고 찾아 준다.
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+    
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
